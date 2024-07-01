2eaf56e19ec64202d8156c55fd73070777c2bb13
gabo4481 <gab65149@gmail.com>
2024-07-01 12:04:37 -0400

    first commit

---
+++ src/test/java/ProyectoG6/ForoAlura/ForoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoG6.ForoAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
